db734761dbee495cf7a867ba9e6a0d32830f386b
bitz161 <bitzgarcia1996@gmail.com>
2022-08-30 23:58:28 +0800

    update the app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import "./App.css";
-import Loading from "./component/loadingScreen/loading.component";
-import { useState } from "react";
 import Navigation from "./routes/navigation/navigation.component";
 
 function App() {
-  const [redirect, setRedirect] = useState(true);
-
-  setTimeout(() => {
-    setRedirect(false);
-  }, 7000);
-
-  return <div className="App">{redirect ? <Loading /> : <Navigation />}</div>;
+  return <Navigation />;
 }
 
 export default App;
