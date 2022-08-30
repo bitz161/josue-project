c62a3bf6f04a2dab286ad623a445ccf4610e2d3a
bitz161 <bitzgarcia1996@gmail.com>
2022-08-31 00:05:33 +0800

    update the current starting point

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import "./App.css";
-import Navigation from "./routes/navigation/navigation.component";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./component/home/home.component";
+import { AnimatePresence } from "framer-motion";
+import OptionNavbar from "./routes/navbar/option-navbar.component";
+import AboutMe from "./component/about/about.component";
+import WorkExperience from "./component/work-experience/work-experience.component";
+import Skills from "./component/skills/skills.component";
+import Education from "./component/education/education.component";
+import ProjectWork from "./component/work/work.component";
+const App = () => {
+  const location = useLocation();
 
-function App() {
-  return <Navigation />;
-}
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<OptionNavbar />}>
+          <Route index element={<Home />} />
+          <Route path="about/" element={<AboutMe />}>
+            <Route index element={<WorkExperience />} />
+            <Route path="skills" element={<Skills />} />
+            <Route path="education" element={<Education />} />
+          </Route>
+          <Route path="project" element={<ProjectWork />}></Route>
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 export default App;
